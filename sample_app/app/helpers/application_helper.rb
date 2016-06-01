8885d4d202250a3c67d65df95abcf3c525fcc056
yukiohnuki <yukiohnuki@yuki-no-MacBook-Pro.local>
2016-06-01 23:16:00 +0900

    第4章

--- sample_app/app/helpers/application_helper.rb
+++ sample_app/app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # ページ毎に完全なタイトルを返す         # ドキュメントとしてのコメント
+  def full_title(page_title = '')                     # メソッド定義とオプション引数
+    base_title = "Ruby on Rails Tutorial Sample App"  # 変数の代入
+    if page_title.empty?                              # 論理値でテスト
+      base_title                                      # 暗黙的な返り値
+    else
+      page_title + " | " + base_title                 # 文字列の結合
+    end
+  end
 end
