2569b5ae83b357e487aa8fdad537033be1f54934
yukiohnuki <yukiohnuki@yuki-no-MacBook-Pro.local>
2016-06-03 22:44:49 +0900

    第１２章

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
-before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+before_action :logged_in_user, only: [:index, :edit, :update, :destroy,:following, :followers]
 before_action :correct_user,   only: [:edit, :update]
 before_action :admin_user,     only: :destroy
 
    def index
     @users = User.where(activated: true).paginate(page: params[:page])
-  end
+   end
 
   def show
     @user = User.find(params[:id])
@@ -16,7 +16,7 @@ before_action :admin_user,     only: :destroy
     @user = User.new
   end
 
-   def create
+  def create
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
@@ -46,6 +46,21 @@ before_action :admin_user,     only: :destroy
     flash[:success] = "User deleted"
     redirect_to users_url
   end
+
+def following
+  @title = "Following" 
+  @user = User.find(params[:id])
+  @users = @user.following.paginate(page: params[:page])
+  render 'show_follow'
+end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
 
 private
 
